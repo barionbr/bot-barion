db6e3483bbe75c8727dfbb6a52583f00b94cfbd5
barionbr <57637524+barionbr@users.noreply.github.com>
2025-05-09 14:24:51 -0300

    barone_bot_github.py

--- barone_bot_github.py
+++ barone_bot_github.py
@@ -5,7 +5,7 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 import random
 
 # Substitua pelo seu token do Bot do Telegram
-TOKEN = 'COLE_SEU_TOKEN_AQUI'
+TOKEN = '7523111564:AAHg_JDcJo81LLlBop40mtgyY8VhSQd0D3g'
 
 # Dicionário para armazenar os números de cada usuário
 usuarios = {}
